ae901c7ebf77aa526336e8b99821e35522d9d9a1
masterUNG <phrombutr@gmail.com>
2016-09-05 14:36:56 +0700

    Test Hub111

--- app/src/main/java/kbu/sineepun/kanpengnet/badmintontraining/TestHub.java
+++ app/src/main/java/kbu/sineepun/kanpengnet/badmintontraining/TestHub.java
@@ -1,10 +1,13 @@
 package kbu.sineepun.kanpengnet.badmintontraining;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
-public class TestHub extends AppCompatActivity {
+
+public class TestHub extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
     private ImageView hub1ImageView, hub2ImageView,
@@ -22,8 +25,54 @@ public class TestHub extends AppCompatActivity {
         hub3ImageView = (ImageView) findViewById(R.id.imvHub3);
         hub4ImageView = (ImageView) findViewById(R.id.imvHub4);
         hub5ImageView = (ImageView) findViewById(R.id.imvHub5);
+        graphImageView = (ImageView) findViewById(R.id.imgGraph);
+
+        //Image Controller
+        hub1ImageView.setOnClickListener(this);
+        hub1ImageView.setOnClickListener(this);
+        hub1ImageView.setOnClickListener(this);
+        hub1ImageView.setOnClickListener(this);
+        hub1ImageView.setOnClickListener(this);
+        graphImageView.setOnClickListener(this);
 
 
     }   // Main Method
+
+    @Override
+    public void onClick(View view) {
+
+        if (view.getId() == R.id.imgGraph) {
+            //Click Graph
+        } else {
+            myIntent(view.getId());
+        }
+
+    }   // onClick
+
+    private void myIntent(int intID) {
+
+        int intResult = 1;
+
+        switch (intID) {
+            case R.id.imvHub1:
+                intResult = 1;
+                break;
+            case R.id.imvHub2:
+                intResult = 2;
+                break;
+            case R.id.imvHub3:
+                intResult = 3;
+                break;
+            case R.id.imvHub4:
+                intResult = 4;
+                break;
+            case R.id.imvHub5:
+                intResult = 5;
+                break;
+        }
+
+        Intent intent = new Intent();
+
+    }   // myIntent
 
 }   // Main Class
